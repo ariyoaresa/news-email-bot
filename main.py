ff8ad53fd82281958a59d5c053fb98973dc83f5b
DECOY55 <sholafunmicod@gmail.com>
2025-12-09 17:02:27 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from email.message import EmailMessage
+
+## Headline + summary scraping
+url = "https://www.bbc.com/news/world"
+
+r = requests.get(url)
+soup = BeautifulSoup(r.text, "html.parser")
+
+headlines = soup.find_all("h2", attrs={"data-testid": "card-headline"})
+summaries = soup.find_all("p")
+
+html_items = ""
+
+## Mail Formatting
+for headline, summary in zip(headlines, summaries):
+    html_items += f"""
+                    <div style="margin-top: 20px; border-bottom: 1px solid;"
+                        <h2 style="margin: 0; font-size: 20px;"><b>{headline.text}</b></h2>
+                            <p style="font-size: 15px;">{summary.text}</p>\n
+                    </div>
+                  """
+
+html = f"""
+            <html>
+                <body style="font-family: Arial, sans-serif;">
+                    <h1>BBC Daily World News</h1>\n
+                        <p>Daily news summaries scraped using Python</p>\n
+                        {html_items}
+                    <p style="margin-top: 40px;">Scraped daily, built by Snax.</p>
+                </body>
+            </html>
+        """
+
+
+## Mail contecnt
+sender_email = "fakee.inc@gmail.com"
+recepient = "sholafunmiademilua@gmail.com"
+subject = "My Daily World News Scraped from BBC"
+
+mail = EmailMessage()
+mail["From"] = sender_email
+mail["To"] = recepient
+mail["Subject"] = subject
+mail.add_alternative(html, subtype="html")
+
+## Smtp server
+smtp_server = "smtp.gmail.com"
+port = 587
+smtp_passwd = "tpgforxzzkyiuxfw"
+
+server = smtplib.SMTP(smtp_server, port)
+server.starttls()
+server.login(sender_email, smtp_passwd)
+
+server.sendmail(sender_email, recepient, mail.as_string())
+
+print("Daily News sent successfully!\n")
